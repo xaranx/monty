c4101bc8cb28036e249e0a6ccc6daf63c08d5826
aefayoub <aef.ayoub@gmail.com>
2023-08-24 06:45:01 -0700

    cmt task 2 - pop

--- monty.h
+++ monty.h
@@ -75,6 +75,7 @@ void free_vglo(void);
 void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **doubly, unsigned int cline);
+void _pop(stack_t **doubly, unsigned int cline);
 
 /*opcodes*/
 void (*list_opcodes(char *opc))(stack_t **stack, unsigned int line_number);
