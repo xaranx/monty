95c069e46294dc81315e3aa7175fbb75d02f9422
aefayoub <aef.ayoub@gmail.com>
2023-08-24 04:57:19 -0700

    cmt header

--- monty.h
+++ monty.h
@@ -74,6 +74,7 @@ void free_vglo(void);
 /* opcode_instructuions*/
 void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
+void _pint(stack_t **doubly, unsigned int cline);
 
 /*opcodes*/
 void (*list_opcodes(char *opc))(stack_t **stack, unsigned int line_number);
